d38028a4226f0b911b8a5d9878aaa7ea319ced79
Dayan Perera <dayannp@gmail.com>
2018-09-30 22:56:08 -0700

    add tests for text area on the blog post

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -19,8 +19,28 @@ it("has a text area and a button", () => {
   expect(wrapped.find("button").length).toEqual(1);
 });
 
-it('has a text area that users can type in',()=>{
-  expect(wrapped.find("textarea").simulate('change',{
-    target:{value:'new comment'}
-  })
-})
+//group the test
+describe("Text area testings ", () => {
+  beforeEach(() => {
+    //First find the text area and simulate the change event
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
+    //then force render the component
+    wrapped.update();
+  });
+
+  it("has a text area that users can type in", () => {
+    //now check wether textarea got the correct value
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("clears the textarea when comment is submitted", () => {
+    //simulate form submit event
+    wrapped.find("form").simulate("submit");
+    //then force render the component
+    wrapped.update();
+
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
