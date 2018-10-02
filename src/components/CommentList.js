ac293f7ed7a4fb962f2db731e3b33f9bbd9ab479
Dayan Perera <dayannp@gmail.com>
2018-10-01 22:50:10 -0700

    create test component for commentlist and instal redux middle ware reduxPromise

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const CommentList = () => {
-  return (
-    <div>
-      <p>Comment List.....</p>
-    </div>
-  );
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map((comment, index) => (
+      <li key={index}>{comment}</li>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    comments: state.comments
+  };
 };
-export default CommentList;
+export default connect(mapStateToProps)(CommentList);
