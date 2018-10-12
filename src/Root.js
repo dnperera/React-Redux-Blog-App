129ead3a11bb5ffaf3d344327f8c84207e1d29c7
Dayan Perera <dayannp@gmail.com>
2018-10-11 19:08:45 -0700

    replace ReduxPromise with custom middle ware async.js and created state validation middle ware using tv4

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
+import async from "middleware/async";
+import stateValidator from "./middleware/stateValidator";
 import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidator)
   );
   return <Provider store={store}>{children}</Provider>;
 };
